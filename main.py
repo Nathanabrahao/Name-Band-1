679bd767f213e45e7a69cbbf86d2a68be8b595f7
Nathanabrahao <nathanntc6@gmail.com>
2024-07-07 16:15:37 -0300

    second

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
+print("Welcome to the band name generator")
+
 city = input("What is name of your city?")
+
 pet = input("What's is name of your pet?")
 
 print("The name of your band would be", city + " " + pet)
